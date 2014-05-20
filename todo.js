3bac2e56785beca3afeba2785f40b28e421fead8
Jon Hilton <jjhilton@gmail.com>
2014-05-20 15:55:28 +0100

    Trying to get underscore.js to work

--- todo.js
+++ todo.js
@@ -5,7 +5,16 @@ function TodoCtrl($scope) {
 		{text:'Learn AngularJs', done:false},
 		{text: 'Build an app', done:false}
 		];
-$scope.totalTodos = $scope.todos.length;
+
+	$scope.getTotalTodos = function () {
+	return $scope.todos.length;
+	}
+
+	$scope.clearCompleted = function () {
+        $scope.todos = _.filter($scope.todos, function(todo){
+            return !todo.done;
+        });
+	};
 
 		$scope.addTodo = function () {
 			$scope.todos.push({text:$scope.formTodoText, done: false});
